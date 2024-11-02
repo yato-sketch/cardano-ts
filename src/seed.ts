bf3a6ecc7c5481d1cabd2f7aeb07ba997717ac64
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-11-02 22:30:03 +0400

    Updated `Seed` class to allow multiple address derivation (#10)

--- src/seed.ts
+++ src/seed.ts
@@ -13,17 +13,30 @@ const getAccountKeyFromSeed = (seed: string) =>
     .derive(2147485463)
     .derive(2147483648);
 
-const getAddressFromSeed = (seed: string, network: Network = "mainnet") => {
+const getAddressFromSeed = (
+  seed: string,
+  network: Network = "mainnet",
+  index: number = 0,
+  includeStakeKey: boolean = true
+) => {
   const accountKey = getAccountKeyFromSeed(seed);
-  const paymentKey = accountKey.derive(0).derive(0).to_raw_key();
-  const stakeKey = accountKey.derive(2).derive(0).to_raw_key();
+  const paymentKey = accountKey.derive(0).derive(index).to_raw_key();
+  const stakeKey = accountKey.derive(2).derive(index).to_raw_key();
   const paymentKeyHash = paymentKey.to_public().hash();
   const stakeKeyHash = stakeKey.to_public().hash();
 
-  return C.BaseAddress.new(
+  if (includeStakeKey)
+    return C.BaseAddress.new(
+      network == "mainnet" ? 1 : 0,
+      C.Credential.new_pub_key(paymentKeyHash),
+      C.Credential.new_pub_key(stakeKeyHash)
+    )
+      .to_address()
+      .to_bech32(undefined);
+
+  return C.EnterpriseAddress.new(
     network == "mainnet" ? 1 : 0,
-    C.Credential.new_pub_key(paymentKeyHash),
-    C.Credential.new_pub_key(stakeKeyHash)
+    C.Credential.new_pub_key(paymentKeyHash)
   )
     .to_address()
     .to_bech32(undefined);
@@ -31,25 +44,37 @@ const getAddressFromSeed = (seed: string, network: Network = "mainnet") => {
 
 const getPrivateKeyFromSeed = (
   seed: string,
-  derivative: number = 0
+  derivative: number = 0,
+  index: number = 0
 ): string => {
   const accountKey = getAccountKeyFromSeed(seed);
-  return accountKey.derive(derivative).derive(0).to_raw_key().to_bech32();
+  return accountKey.derive(derivative).derive(index).to_raw_key().to_bech32();
 };
 
 class Seed {
+  private index: number = 0;
+
   constructor(private phrase: string, private network: Network = "testnet") {}
 
-  getAddress() {
-    return getAddressFromSeed(this.phrase, this.network);
+  next() {
+    this.index++;
+  }
+
+  getAddress(includeStakeKey: boolean = true) {
+    return getAddressFromSeed(
+      this.phrase,
+      this.network,
+      this.index,
+      includeStakeKey
+    );
   }
 
   getPrivateKey() {
-    return getPrivateKeyFromSeed(this.phrase);
+    return getPrivateKeyFromSeed(this.phrase, 0, this.index);
   }
 
   getPrivateStakeKey() {
-    return getPrivateKeyFromSeed(this.phrase, 2);
+    return getPrivateKeyFromSeed(this.phrase, 2, this.index);
   }
 }
 
